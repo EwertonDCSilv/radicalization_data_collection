51658434b7096d4afefa140dab64431efbe38e9d
manoelhortaribeiro <manoelhortaribeiro@gmail.com>
2019-04-17 22:18:01 -0300

    Updates number of pages.

--- forums_tools/incels_co_loader.py
+++ forums_tools/incels_co_loader.py
@@ -32,7 +32,7 @@ def build_index(dst, nump):
 
     df_list = []
 
-    for page_num in range(1, 20 + 1, 1):
+    for page_num in range(1, number_of_pages + 1, 1):
         print("Page {0}/{1}".format(page_num, number_of_pages))
         r = session.get(INCELS_URL + str(page_num))
 
