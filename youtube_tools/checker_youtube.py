81ced7bda36bd8dc29734f1ee9812144d84c4a78
manoelhortaribeiro <manoelhortaribeiro@gmail.com>
2019-04-24 14:08:16 -0300

    Improves script to check videos metadata.

--- youtube_tools/checker_youtube.py
+++ youtube_tools/checker_youtube.py
@@ -2,6 +2,7 @@ import pandas.errors as pderr
 import pandas as pd
 import argparse
 import json
+import glob
 import os
 
 parser = argparse.ArgumentParser(description="""""")
@@ -9,8 +10,11 @@ parser = argparse.ArgumentParser(description="""""")
 parser.add_argument("--src", dest="src", type=str, default="./data/youtube/yt/",
                     help="Source folder created by `loader_youtube.py`")
 
-parser.add_argument("--graph", dest="graph", type=str, default="./data/youtube/rc/recommended_20190416Apr.jsonl",
+parser.add_argument("--graph", dest="graph", type=str, default="./data/youtube/rc/recommended_20190422Apr.jsonl",
                     help="Source folder created by `loader_youtube.py`")
+
+parser.add_argument("--src_csv", dest="src_csv", type=str, default="./data/youtube/sources.csv",
+                    help="A .csv containing rows `Name`, `Category`, `Data Collection step`, `Id`")
 
 args = parser.parse_args()
 
@@ -20,9 +24,13 @@ def error_msg(channel_id, name, error_msg):
     print("-" * 60)
 
 
+graph_channels = set()
+yt_channels = set()
+
 with open(args.graph) as f:
     for line in f.readlines():
         channel = json.loads(line)
+        graph_channels.add(channel["channel_id"])
 
         try:
             df = pd.read_csv(args.src + channel["channel_id"] + ".csv")
@@ -41,3 +49,17 @@ with open(args.graph) as f:
         except pderr.EmptyDataError:
             os.remove(args.src + channel["channel_id"] + ".csv")
             error_msg(channel["channel_id"], channel["name"], "Empty")
+
+df = pd.read_csv(args.src_csv)
+
+src_channels = set(list(df["Id"].values))
+
+for file in glob.glob(args.src + "*"):
+    yt_channels.add(file.split("/")[-1][:-4])
+
+print("In SRC but not in YT:  ", src_channels - yt_channels)
+print("In YT but not in SRC:  ", yt_channels - src_channels)
+print("In GRAPH but not in YT:  ", graph_channels - yt_channels)
+print("In YT but not in GRAPH:  ", yt_channels - graph_channels)
+print("In SRC but not in GRAPH:", src_channels - graph_channels)
+print("In GRAPH but not in SRC:", graph_channels - src_channels)
