345b12f5c97b0ed1677b97ead4c1e90c7d5500c5
EwertonDCSilv <ewertonsilva@dcc.ufmg.br>
2019-04-30 01:04:48 -0300

    Add new files

---
+++ forums_tools/list.bat
@@ -0,0 +1,6 @@
+cp incels_co_loader.py no-love_sky_loader.py
+cp incels_co_loader.py no-mgtow_loader.py
+cp incels_co_loader.py no-pick-up-artist_loader.py
+cp incels_co_loader.py no-redpilltalk_loader.py
+cp incels_co_loader.py no-rooshv_loader.py
+cp incels_co_loader.py no-theattraction_loader.py
--- forums_tools/no-rooshv_loader.py
+++ forums_tools/no-rooshv_loader.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+import dateutil.relativedelta
 from requests_html import HTMLSession
-from multiprocessing import Pool
+#from multiprocessing import Pool
 from bs4 import BeautifulSoup
+import multiprocessing as mp
 import pandas as pd
 import re
 import os
@@ -9,7 +12,6 @@ LINKS_REGEX = r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-
 INCELS_URL = "https://incels.co/forums/inceldom-discussion.2/page-"
 INCELS_THREAD_BASE = "https://incels.co"
 
-
 def get_html_session(session=None):
     if session is None:
         print("new html session")
@@ -17,10 +19,69 @@ def get_html_session(session=None):
     else:
         return session
 
+def handle_date(datePost):
 
-def handle_date():
-    pass
+    weekDayList = {
+        "Monday": 0,
+        "Tuesday": 1,
+        "Wednesday": 2,
+        "Thursday": 3,
+        "Friday": 4,
+        "Saturday": 5,
+        "Sunday": 6,
+        "Yesterday": -1,
+        "Today": -1
+    }
+        
+    monthLista = {
+        "Jan": 1,
+        "Feb": 2,
+        "Mar": 3,
+        "Apr": 4,
+        "May": 5,
+        "Jun": 6,
+        "Jul": 7,
+        "Aug": 8,
+        "Sep": 9,
+        "Oct": 10,
+        "Nov": 11,
+        "Dec": 12,
+    }
 
+    datePost = datePost.replace(",","")
+    weekDay = datePost.split()
+
+    currentDate = datetime.today()
+    realDate = datetime.today()
+    currentDate = currentDate.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    if(weekDay[0] in weekDayList):
+    
+        if(weekDayList[weekDay[0]] > currentDate.today().weekday() and weekDayList[weekDay[0]] != -1):
+            numberDay = (7-weekDayList[weekDay[0]]) + currentDate.today().weekday()
+            realDate = currentDate - dateutil.relativedelta.relativedelta(days=numberDay)
+
+        elif(weekDayList[weekDay[0]] < currentDate.today().weekday() and weekDayList[weekDay[0]] != -1):
+            numberDay = currentDate.today().weekday() - weekDayList[weekDay[0]]
+            realDate = currentDate - dateutil.relativedelta.relativedelta(days=numberDay)
+        
+        elif("Yesterday" in weekDay[0]):
+            realDate = currentDate - dateutil.relativedelta.relativedelta(days=1) 
+        
+        elif("Today" in weekDay[0]):
+            realDate = currentDate - dateutil.relativedelta.relativedelta(days=0) 
+        
+        if('am' in datePost or "12:" in datePost):
+            weekDayHour = weekDay[2].split(':')
+            realDate = realDate.replace(hour=int(weekDayHour[0]), minute=int(weekDayHour[1]))
+        else:
+            weekDayHour = weekDay[2].split(':')
+            realDate = realDate.replace(hour=int(weekDayHour[0])+12, minute=int(weekDayHour[1]))
+
+    else:
+        realDate = realDate.replace(day=int(weekDay[1]), month=int(monthLista[weekDay[0]]), year=int(weekDay[2]),hour=0, minute=0, second=0, microsecond=0)
+    
+    return realDate
 
 def build_index(src, dst, nump):
     session = get_html_session()
@@ -62,7 +123,6 @@ def build_index(src, dst, nump):
 
     df.to_csv(dst)
 
-
 def get_thread(link, session=None):
     session = get_html_session(session)
     number_of_pages_post = get_num_pages_post(link, session)
@@ -76,7 +136,6 @@ def get_thread(link, session=None):
     df = pd.DataFrame(df_list)
     df.to_csv("./data/forums/incels/posts/" + re.sub("/", "", link[9:]) + ".csv", index=False)
 
-
 def get_num_pages_post(link, session=None):
     session = get_html_session(session)
     r_post = session.get(INCELS_THREAD_BASE + link)
@@ -86,22 +145,21 @@ def get_num_pages_post(link, session=None):
         number_of_pages_post = 1
     return number_of_pages_post
 
-
 def get_posts_page(link, thread_page, session=None):
     session = get_html_session(session)
     r_post = session.get(INCELS_THREAD_BASE + link + "page-" + str(thread_page))
     return r_post.html.find('.message--post')
 
-
 def get_post(post, link, session=None):
     number_blockquotes = post.find('.message-content')[0].html.count("</blockquote>")
     bs_text = BeautifulSoup(post.find('.message-content')[0].html, "lxml")
+    
     for i in range(number_blockquotes):
         bs_text.blockquote.decompose()
 
     post_dict = {
         "author": post.find('.username', first=True).text,
-        "link_author": list(post.find('.username', first=True).links)[0],
+        #"link_author": list(post.find('.username', first=True).links)[0],
         "resume_author": post.find('.message-userTitle', first=True).text,
         "joined_author": post.find('.message-userExtras dd', first=True).text,
         "messages_author": int(re.sub(",", "", post.find('.message-userExtras dd')[1].text)),
@@ -111,27 +169,26 @@ def get_post(post, link, session=None):
         "id_post": re.sub("js-post-", "", post.attrs["id"]),
         "id_post_interaction": [re.sub(r'/goto/post\?id=', "", list(v.links)[0])
                                 for v in post.find(".bbCodeBlock-title")],
-        "date_post": post.find('.u-concealed')[0].text,
+        "date_post": handle_date(post.find('.u-concealed')[0].text),
         "links": re.findall(LINKS_REGEX, str(bs_text.html)),
         "thread": link
     }
     return post_dict
 
-
 if __name__ == "__main__":
     import argparse
-
 
     def get_thread_global(link):
         if session_global is not None:
             session = session_global
-        get_thread(link, session)
-
+        #get_thread(link, session)
+        print("{}\n".format(link))
+        print("{}\n".format(session))
+        exit()
 
     def initialize_worker():
         global session_global
         session_global = HTMLSession()
-
 
     parser = argparse.ArgumentParser(description="""""")
 
@@ -159,9 +216,29 @@ if __name__ == "__main__":
 
     else:
         to_run = list(pd.read_csv(args.index)["link"].values)
+    
         if args.debug:
-            for i in to_run:
-                get_thread(to_run)
+            for link in to_run:
+                get_thread(link)
         else:
-            p = Pool(args.nump, initializer=initialize_worker)
-            p.map(get_thread_global, to_run)
+            #p = Pool(args.nump, initializer=initialize_worker)
+            #for link in to_run:
+                #p.map(get_thread_global, link)
+                #print("AAAAAAA")
+
+            processes = []
+            k = 0
+
+            for link in to_run:
+                if k == 4:
+                    break
+                processes.append(mp.Process(target=get_thread_global, args=(link)))
+                k = k + 1
+    
+            # Run processes
+            for p in processes:
+                p.start()
+
+            # Exit the completed processes
+            for p in processes:
+                p.join()
